680579896d46b4540607523f8d60c592231dffe4
13am028 <71486982+13am028@users.noreply.github.com>
2023-01-30 19:48:11 +0700

    More refactoring/precommit/lint

--- src/app.py
+++ src/app.py
@@ -1,27 +1,67 @@
+"""Create and serve app."""
 import os
 
+import flask.app
 from flask import Flask
 from sqlalchemy_utils import create_database, database_exists
-from web import web
+from waitress import serve
+
 from db import db
+from location import location
+from pages import page
+from product import product
+from stock import stock
 
-url = "postgresql://" + os.environ["POSTGRES_USER"] + ":" + os.environ[
-    "POSTGRES_PASSWORD"] + "@" + os.environ["POSTGRES_DB"] + "/stock"
+url = (
+    "postgresql://"
+    + os.environ["POSTGRES_USER"]
+    + ":"
+    + os.environ["POSTGRES_PASSWORD"]
+    + "@"
+    + os.environ["POSTGRES_DB"]
+    + "/stock"
+)
 
-# url = "postgresql://" + "dbc" ":" + "dbc" + "@" + "localhost:5434" + "/stock"
-if not database_exists(url):
-    create_database(url)
 
-app = Flask(__name__)
-app.app_context().push()
-app.config["SQLALCHEMY_DATABASE_URI"] = url
+def create_db() -> None:
+    """Create database tables if not exist."""
+    if not database_exists(url):
+        create_database(url)
 
-if __name__ == "__main__":
+
+def create_app() -> flask.app.Flask:
+    """Create Flask app."""
+    app: flask.app.Flask = Flask(__name__)
+    app.app_context().push()
+    app.config["SQLALCHEMY_DATABASE_URI"] = url
+    return app
+
+
+def init_app_db(app: flask.app.Flask) -> None:
+    """Initiate database to app."""
     db.init_app(app)
     with app.app_context():
         db.create_all()
         db.session.commit()
-    app.register_blueprint(web)
-    from waitress import serve
 
+
+def register_blueprint(app: flask.app.Flask) -> None:
+    """Register blueprints to app."""
+    app.register_blueprint(page)
+    app.register_blueprint(location)
+    app.register_blueprint(product)
+    app.register_blueprint(stock)
+
+
+def serve_app(app: flask.app.Flask) -> None:
+    """Serve the app using waitress."""
     serve(app, host="0.0.0.0", port=7777)
+
+
+if __name__ == "__main__":
+    create_db()
+    app: flask.app.Flask = create_app()
+    print(type(app))
+    init_app_db(app)
+    register_blueprint(app)
+    serve_app(app)
---
+++ src/product.py
@@ -0,0 +1,25 @@
+"""Route and methods for products."""
+from flask import Blueprint, Response, redirect, request, url_for
+
+from db import Products, session
+
+product = Blueprint("product", __name__)
+
+
+@product.route("/add-product", methods=["POST"])
+def add_product() -> Response:
+    """Add a new product."""
+    new_product: Products = Products(product_name=request.form["product"])
+    session.add(new_product)
+    session.commit()
+    return redirect(url_for("page.products"))
+
+
+@product.route("/delete-product", methods=["DELETE"])
+def delete_product() -> Response:
+    """Delete product."""
+    pid: str = request.form["pid"]
+    del_product = Products.query.filter(Products.id == pid).first()
+    session.delete(del_product)
+    session.commit()
+    return redirect(url_for("page.products"))
